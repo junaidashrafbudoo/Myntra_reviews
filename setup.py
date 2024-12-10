bbffbf1a1b04f1bebc28a94f8999700ee7019b47
Junaid Ashraf Budoo <junaidashraf131@gmail.com>
2024-12-10 16:01:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = "Myntra Scrapper",
+    version = "0.0.1",
+    author = "Junaid",
+    author_email = "junaidashraf.cqai@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
